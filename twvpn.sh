a4d107418561bc17d2d01e498af9d88dd418902f
Bang Wu <ohwubang@gmail.com>
2018-12-02 16:40:50 +0800

    read the password from the keychian

--- twvpn.sh
+++ twvpn.sh
@@ -3,7 +3,12 @@
 killall 'Cisco AnyConnect Secure Mobility Client' 2>/dev/null
 /opt/cisco/anyconnect/bin/vpn disconnect >/dev/null
 
+AUTH_CODE=$(security find-generic-password -a auth_code -s playground -w)
+USER_NAME=$(security find-generic-password -a user_name -s playground -w)
+PASSWORD=$(security find-generic-password -a password -s playground -w)
+VALIDATE_TYPE=$(security find-generic-password -a validate_type -s playground -w)
 code=`oathtool --totp -b $AUTH_CODE`
+
 
 /opt/cisco/anyconnect/bin/vpn -s connect $VPN_URL << EOF | sed 's/Password: .*/Password: ********/g'
 $USER_NAME
